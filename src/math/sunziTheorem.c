f458dcd7e1259c1fc50ebfd3f6dfac81810bf293
barkowal <b.kowalski1@o365.us.edu.pl>
2024-04-10 10:04:45 +0200

    Minor congruence changes

--- src/math/sunziTheorem.c
+++ src/math/sunziTheorem.c
@@ -4,8 +4,8 @@
 
 //xi = a * mi * bi 
 //bi = (mi^-1 mod m) OR NWD(singleMod, mi) = a * a + bi * singleMod 
-int calculatePart(int a, int singleMod, int sumMod){
-	int mi = sumMod/singleMod;
+int calculatePart(int a, int singleMod, int mulMod){
+	int mi = mulMod/singleMod;
 	int bi, x;
 	ext_nwd(singleMod, mi, &x, &bi);
 	return a*mi*bi;
@@ -13,26 +13,26 @@ int calculatePart(int a, int singleMod, int sumMod){
 
 
 int calculateSunziTheorem(int a[], int singleMod[], int size){
-	int sumMod = 1;
+	int mulMod = 1;
 	int result = 0;
 	int i;
 	for(i=0; i<size; i++){
-		sumMod *= singleMod[i];
+		mulMod *= singleMod[i];
 	}
 	
-	if(!checkInputValues(a, singleMod, sumMod, size))
+	if(!checkInputValues(a, singleMod, mulMod, size))
 		return 0;
 
 	for(i=0; i<size; i++){
-		result += calculatePart(a[i], singleMod[i], sumMod);
+		result += calculatePart(a[i], singleMod[i], mulMod);
 	}
-	result %= sumMod;
+	result %= mulMod;
 
-	//To get result in {0, ..., sumMod - 1}
+	//To get result in {0, ..., mulMod - 1}
 	while(result<0)
-		result += sumMod;
-	while(result>=sumMod)
-		result -= sumMod;
+		result += mulMod;
+	while(result>=mulMod)
+		result -= mulMod;
 
 	return result;
 }
@@ -40,11 +40,11 @@ int calculateSunziTheorem(int a[], int singleMod[], int size){
 
 //Algorithm with a little bit of tweaking should work with any values,
 //but for now it only works with pairwise coprimes
-int checkInputValues(int a[], int moduloValues[], int sumMod, int size){
+int checkInputValues(int a[], int moduloValues[], int mulMod, int size){
 	int i,j;
 
 	for(i=0; i<size; i++){
-		if(a[i] >= sumMod || a[i] < 0) return 0;
+		if(a[i] >= mulMod || a[i] < 0) return 0;
 
 		for(j=i+1; j<size; j++){
 			if(nwd(moduloValues[i], moduloValues[j]) != 1)
